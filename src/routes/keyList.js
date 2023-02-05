549c2536b24ed3f8e847c476e33cc15f1b3e51e8
Serhii Ryzhykh <serhiiryzhykh@Serhiis-MacBook-Air.local>
2023-02-05 13:10:08 +0200

    added simple server rules

--- src/routes/keyList.js
+++ src/routes/keyList.js
@@ -4,8 +4,9 @@ import fs from 'fs'
 export const keyListRouter = express()
 
 const rawData = fs.readFileSync('data.json');
-const lists = JSON.parse(rawData).lists
+const lists = JSON.parse(rawData)
 
 keyListRouter.get('/api/keyList', (req, res) => {
+    console.log('TEST')
     res.send(lists);
 });
